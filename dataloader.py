231a3bc489109adf72b817b6578cf7a4fff4e92a
go13 <dima.blinkov@gmail.com>
2024-11-14 21:04:54 +0000

    cleanup

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 import torch
+from transformer_common import TransformerConfig
+
 
 
 def read_and_merge_csv_files(directory_path, filenames, start_date='2010-01-01', end_date='2020-12-31'):
@@ -48,6 +50,44 @@ def read_and_merge_csv_files(directory_path, filenames, start_date='2010-01-01',
     data = data.fillna(method='bfill').reset_index(drop=True)
 
     return data, found_files
+
+
+class GenericDataloader:
+
+    def __init__(self, config: TransformerConfig, in_data, out_data):
+
+        self.config = config
+        self.in_data = in_data.to(self.config.my_device).to(config.precision)
+        self.out_data = out_data.to(self.config.my_device).to(config.precision)
+        self.config = config
+
+        n = int(0.9 * len(self.in_data))  # first 90% will be trained, rest val
+        self.in_train_data = self.in_data[:n]
+        self.in_val_data = self.in_data[n:]
+
+        self.out_train_data = self.out_data[:n]
+        self.out_val_data = self.out_data[n:]
+
+    def get_batch(self, split):
+        # generate a small batch of data of inputs x and targets y
+        in_data = self.in_train_data if split == 'train' else self.in_val_data
+        out_data = self.out_train_data if split == 'train' else self.out_val_data
+
+        ix = torch.randint(len(in_data) - self.config.block_size, (self.config.batch_size,))
+
+        x = torch.stack([in_data[i:i + self.config.block_size] for i in ix])
+        y = torch.stack([out_data[i + 1:i + self.config.block_size + 1] for i in ix])
+
+        x, y = x.to(self.config.my_device), y.to(self.config.my_device)
+
+        # print(x.shape, y.shape)
+        return x, y
+
+    def get_train_batch(self):
+        return self.get_batch('train')
+
+    def get_val_batch(self):
+        return self.get_batch('test')
 
 
 class TimeseriesDataloader(object):
